3ff5bf6705bb2696bf402c99d169142eea598c22
Lamia Haider <lamia.haider@gmail.com>
2020-06-29 18:37:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,39 +2,60 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-  def self.scrape_index_page(index_url)
+
+  def self.scrape_index_page(url)
     students = []
-    html = open("./fixtures/student-site/index.html")
-    doc = Nokogiri::HTML(html)
-    doc.css(".student-card").each do |scard|
-      name = scard.css("h4.student-name").text
-      location = scard.css("p.student-location").text
-      url = scard.css("a").attribute("href").value
-      students << {:name => name, :location => location, :profile_url => url}
+    d = Nokogiri::HTML(open(url))
+    d.css('.student-card').collect do |sc|
+      student_name = sc.css('.student-name').text
+      student_location = sc.css('.student-location').text
+      student_profile_link = "#{sc.attr('href')}"
+
+      binding.pry
     end
-    students
-  end
-
-  def self.scrape_profile_page(profile_url)
-    html = open(profile_url)
-    doc = Nokogiri::HTML(html)
-    student = {}
-    sites = doc.css(".social-icon-container a").collect {|sic| sic.attribute("href").value}
-    sites.each do |url|
-      if url.include?("twitter")
-        student[:twitter] = url
-      elsif url.include?("linkedin")
-        student[:linkedin] = url
-      elsif url.include?("github")
-        student[:github] = url
-      else student[:blog] = url
-      end
-      student[:profile_quote] = doc.css(".profile-quote").text
-      student[:bio] = doc.css(".bio-block .bio-content .description-holder p").text
-
-    end
-    student
-
   end
 
 end
+
+
+
+
+
+#
+# class Scraper
+#   def self.scrape_index_page(index_url)
+#     students = []
+#     html = open("./fixtures/student-site/index.html")
+#     doc = Nokogiri::HTML(html)
+#     doc.css(".student-card").each do |scard|
+#       name = scard.css("h4.student-name").text
+#       location = scard.css("p.student-location").text
+#       url = scard.css("a").attribute("href").value
+#       students << {:name => name, :location => location, :profile_url => url}
+#     end
+#     students
+#   end
+#
+#   def self.scrape_profile_page(profile_url)
+#     html = open(profile_url)
+#     doc = Nokogiri::HTML(html)
+#     student = {}
+#     sites = doc.css(".social-icon-container a").collect {|sic| sic.attribute("href").value}
+#     sites.each do |url|
+#       if url.include?("twitter")
+#         student[:twitter] = url
+#       elsif url.include?("linkedin")
+#         student[:linkedin] = url
+#       elsif url.include?("github")
+#         student[:github] = url
+#       else student[:blog] = url
+#       end
+#       student[:profile_quote] = doc.css(".profile-quote").text
+#       student[:bio] = doc.css(".bio-block .bio-content .description-holder p").text
+#
+#     end
+#     student
+#
+#   end
+#
+# end
